5ea394c9f0f8c96c7c81dc8492526b374ff59c07
BuildTools <unconfigured@null.spigotmc.org>
2020-02-19 06:09:01 +0100

    Creation of the project

---
+++ config/configLocal.py
@@ -0,0 +1,4 @@
+apiKey = "p8Fr9OUaInfCKE8eAQHctJym6"
+apiSecret = "a9JGF7mCu6S3jvG0EKIrxxjbd9efVWDN7OMT8kcxLu0IRoZjOP"
+accessToken = "1229933414884749312-MS1qQ9x9DuCBpRewnzogCjgXZAmReX"
+accessTokenSecret = "FCWCZ3ktNM1v18IU6sgLc8AHmSHx2NRk6G3omj5QcpcbE"
---
+++ src/main.py
@@ -0,0 +1,65 @@
+import tweepy
+import threading
+from random import randint
+from config import configLocal as cfg
+
+voyelle = "a", "e", "é", "ê", "è", "i", "o", "u", "y"
+
+verbs = open("../resources/verbs.txt", "r", encoding="utf-8")
+male = open("../resources/male.txt", "r", encoding="utf-8")
+feminine = open("../resources/feminine.txt", "r", encoding="utf-8")
+adjectifMale = open("../resources/adjectifMale.txt", "r", encoding="utf-8")
+adjectifFeminine = open("../resources/adjectifFeminin.txt", "r", encoding="utf-8")
+
+verbsLines = verbs.readlines()
+maleLines = male.readlines()
+feminineLines = feminine.readlines()
+adjMaleLines = adjectifMale.readlines()
+adjFemininLines = adjectifFeminine.readlines()
+
+auth = tweepy.OAuthHandler(cfg.apiKey, cfg.apiSecret)
+auth.set_access_token(cfg.accessToken, cfg.accessTokenSecret)
+
+api = tweepy.API(auth)
+
+try:
+    api.verify_credentials()
+    print("Authentication OK")
+except:
+    print("Error during authentication")
+
+
+def generateVerb():
+    randVerbs = randint(0, len(verbsLines) - 1)
+    verb = verbsLines[randVerbs]
+
+    if verb.startswith(voyelle):
+        return "j\'" + verb.rstrip()
+    else:
+        return "je " + verb.rstrip()
+
+
+def generateWord(withAdj):
+    int = randint(0, 1)
+    adj = ""
+
+    if withAdj == True & int == 1:
+        adj = " " + adjMaleLines[randint(0, len(adjMaleLines))].rstrip()
+    elif withAdj == True & int == 0:
+        adj = " " + adjFemininLines[randint(0, len(adjFemininLines))].rstrip()
+
+    if int == 1:
+        return " un " + maleLines[randint(0, len(maleLines)) - 1].strip() + adj
+    else:
+        return " une " + feminineLines[randint(0, len(feminineLines)) - 1].strip() + adj
+
+
+def postTweet():
+    threading.Timer(3600.0, postTweet).start()
+    tweet = (generateVerb() + generateWord(False) + " et "
+             + generateVerb() + generateWord(True) + ".".capitalize())
+    api.update_status(tweet)
+    print("Le tweet \"" + tweet + "\" à bien été posté")
+
+
+postTweet()
